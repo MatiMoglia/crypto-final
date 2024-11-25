13969bdaf62e983cbe98f242a2227dc03a48507e
Matias Moglia <matimoglia951@gmail.com>
2024-11-24 23:31:58 -0300

    Se incorporo Toastify (notificaciones)

--- src/main.js
+++ src/main.js
@@ -2,7 +2,20 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Toastify from 'vue3-toastify';
+import "vue3-toastify/dist/index.css"; 
 import "aos/dist/aos.css";
-import "./assets/styles.css"
+import "./assets/styles.css";
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+app.use(Toastify, {
+  position: "top-right", 
+  timeout: 3000,       
+  closeOnClick: true,   
+  pauseOnHover: true,   
+});
+
+app.mount('#app');
