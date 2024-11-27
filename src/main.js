1cbcfc37dc4b0eec298214f9ad75bf71bd53c4d5
Matias Moglia <matimoglia951@gmail.com>
2024-11-26 23:04:29 -0300

    cambio de ruta styles.css

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store';
 import Toastify from 'vue3-toastify';
 import "vue3-toastify/dist/index.css"; 
 import "aos/dist/aos.css";
-import "./assets/styles.css";
+import "../src/assets/css/styles.css";
 
 const app = createApp(App);
 
